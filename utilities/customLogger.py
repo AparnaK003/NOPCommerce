808515d8d9895c8540a0b958f14c3ab2998da2ac
Aparna <aparnakishore003@gmail.com>
2023-09-29 02:27:17 +0530

    first commit

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -5,7 +5,7 @@ class LogGen:
     def loggen():
 
         logging.basicConfig(filename=".\\Logs\\automation.log",
-                            format='%(asctime)s %(levelname)s %(message)s', datefmt="%m/%d/%Y %I:%M:%S %p")
+                            format='%(asctime)s %(levelname)s %(message)s', datefmt="%m/%d/%Y %I:%M:%S %p", force=True)
 
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
